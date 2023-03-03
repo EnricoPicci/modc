26c3785c3c7716a899510a32a4b29207cfd9b48d
Picci <enrico.piccinin@gmail.com>
2023-03-03 18:20:48 +0100

    code for v1.3.0

---
+++ pkgmodc/apis-call-int.go
@@ -0,0 +1,12 @@
+package pkgmodc
+
+import (
+	"fmt"
+
+	"github.com/EnricoPicci/modc/intapis"
+)
+
+func ApiThatCallsInternalApi() {
+	fmt.Println("I am an API of module C that calls an internal API of module C")
+	intapis.InternalAPI()
+}
--- pkgmodc/apis-call-modd.go
+++ pkgmodc/apis-call-modd.go
@@ -3,14 +3,8 @@ package pkgmodc
 import (
 	"fmt"
 
-	"github.com/EnricoPicci/modc/intapis"
 	"github.com/EnricoPicci/modd/pkgmodd"
 )
-
-func ApiThatCallsInternalApi() {
-	fmt.Println("I am an API of module C that calls an internal API of module C")
-	intapis.InternalAPI()
-}
 
 func ApiThatCallsModuleDApi() {
 	fmt.Println("I am an API of module C that calls an an API of module D")
